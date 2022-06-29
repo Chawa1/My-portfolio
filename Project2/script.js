a354fe88cdaa9ca00f4a23c73a03917165df5b10
Chawa1 <chawankhalil11@gmail.com>
2022-06-29 19:27:09 +0300

    feat: add Project2

--- Project2/script.js
+++ Project2/script.js
@@ -1,3 +1,13 @@
-alert('slaw');
+function clickHandler(){
+let randomColor=generatedRandomColor();
 
+document.getElementById('container').style.backgroundColor=`#${randomColor}`
+}
 
+function generatedRandomColor(){
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+return randomColor;
+}
+
+/* bo away dway chand chrkaayak otomatiky xoy rang bgoret */
+setInterval(clickHandler, 200);
